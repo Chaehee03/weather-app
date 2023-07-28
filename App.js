51808ac3688227851a1669751783cbfc6a08370e
Song Chaehee <sch003322@gmail.com>
2023-07-28 21:52:50 +0900

    feat: 메인 페이지 구현

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.location}>
+        <Text style={styles.locationName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <Text style={styles.temperature}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#bdcebe",
+  },
+  location: {
+    flex: 1.25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  locationName: {
+    fontSize: 60,
+    fontWeight: "450s",
+  },
+  weather: {
+    flex: 3,
+    alignItems: "center",
+  },
+  temperature: {
+    marginTop: 100,
+    fontSize: 100,
+  },
+  description: {
+    fontSize: 40,
   },
 });
